64add36631a2fef44031020e9c1333e49f0f2b80
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-02-11 18:29:58 -0600

    Updated Level.h to match the XML Level Files
    Updated Level.h to better match the Level XML File
    example.  Added a header file with object includes
    for Level.h.

--- game_engine/include/Level.h
+++ game_engine/include/Level.h
@@ -25,24 +25,45 @@ class level{
 		level(std::string &n);
 		level(const level &l);
 		~level();
-
-		void add_mesh(std::shared_ptr<mesh> &obj);
-		void add_mesh(std::string &url, std::vector<transform> &transforms);
-		void add_mesh(std::vector<std::shared_ptr<mesh> > &objs);
 		
-		void add_light(std::shared_ptr<light> &light);
-		void add_lights(std::vector<std::shared_ptr<light> > &lights);
+		// ADD OBJECTS
+		void add_cam(); // Camera Constructors
+		void add_cam(std::string name);
+		void add_cam(const camera &cam);
+		
+		void add_light(); // Light Constructors
+		void add_light(std::string &name);
+		void add_light(std::string &name, std::vector<double> &loc);
 
-		void clean();
-
-		void get_name(std::string &dest);
-		void get_object(uint32 index, object &dest);
-		void get_cam_coords(std::vector<double> &dest);
-		void get_cam_direction(std::vector<double> &dest);
-
-		void rmv_obj(uint32 index);
-		void set_name(std::string &new_name);
-		void set_light_color(uint32 index, std::vector<double> values);
+		void add_mesh(); // Mesh Constructors
+		void add_mesh(std::string &name);
+		void add_mesh(std::string &name, std::string &mesh_path);
+		void add_mesh(std::string &name, std::string &mesh_path, std::string &mat_path);
+		void add_mesh(const mesh &mesh);
+		
+		void set_name(std::string &n);
+		void set_cam_name(uint32 index, std::string &n);
+		void set_light_name(uint32 index, std::string &n);
+		void set_mesh_name(uint32 index, std::string &n);
+		
+		std::vector<std::shared_ptr<camera> > * get_cams();
+		std::vector<std::shared_ptr<light> > * get_lights();
+		std::vector<std::shared_ptr<mesh> > * get_meshes();
+		
+		// Camera Object Functions
+		void set_cam_clip(uint32 index, std::vector<double> &clip);
+		void set_cam_loc(uint32 index, std::vector<double> &location);
+		void set_cam_target(uint32 index, std::vector<double> &target);
+		
+		// Light Object Functions
+		void set_light_color(uint32 index, std::vector<double> &color);
+		void set_light_loc(uint32 index, std::vector<double> &location);
+		
+		// Mesh Object Functions
+		void add_mesh_transform(uint32 index, TRANSF t, std::vector<double> &transform_vector);
+		void rmv_mesh_transform(uint32 index, uint32 transform_index);
+		void set_mesh_matpath(uint32 index, std::string &path);
+		void set_mesh_path(uint32 index, std::string &path);
 };
 
 #endif /* LEVEL_H_ */
