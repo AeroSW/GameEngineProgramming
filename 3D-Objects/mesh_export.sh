e09e77d37bfad056a780cdd6b2035df20da79e72
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-02-16 02:04:31 -0600

    Updated to have Correct Ogre Paths

--- 3D-Objects/mesh_export.sh
+++ 3D-Objects/mesh_export.sh
@@ -1,5 +1,5 @@
-/home/uge/Documents/Projects/Class/4903_GE/ogre_src_v1-8-1/bin/OgreXMLConverter ./XML-Files/$1.mesh.xml
-/home/uge/Documents/Projects/Class/4903_GE/ogre_src_v1-8-1/bin/OgreMeshUpgrader ./XML-Files/$1.mesh
+/usr/local/bin/OgreXMLConverter ./XML-Files/$1.mesh.xml
+/usr/local/bin/OgreMeshUpgrader ./XML-Files/$1.mesh
 mv ./XML-Files/$1.mesh ./Mesh-Files/
 mv ./OgreMeshUpgrade.log ./$1MeshUpgrade.log
 mv ./$1MeshUpgrade.log ./Log-Files/
