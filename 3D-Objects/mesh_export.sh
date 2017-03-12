dfe7a7369931e916a919c2c0873992159aaf157e
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-03-12 00:10:11 -0500

    Ignoring 3D-Objects Directory.

--- 3D-Objects/mesh_export.sh
+++
@@ -1,7 +0,0 @@
-/usr/local/bin/OgreXMLConverter ./XML-Files/$1.mesh.xml
-/usr/local/bin/OgreMeshUpgrader ./XML-Files/$1.mesh
-mv ./XML-Files/$1.mesh ./Mesh-Files/
-mv ./OgreMeshUpgrade.log ./$1MeshUpgrade.log
-mv ./$1MeshUpgrade.log ./Log-Files/
-mv ./OgreXMLConverter.log ./$1XMLConverter.log
-mv ./$1XMLConverter.log ./Log-Files/
