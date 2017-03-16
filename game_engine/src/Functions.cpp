723cfd7ad9fae00dd3f2af6de73e13db1a7b1c28
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-03-16 09:12:02 -0500

    Fixed minor compilation errors in Functions.cpp and Game.cpp

--- game_engine/src/Functions.cpp
+++ game_engine/src/Functions.cpp
@@ -36,12 +36,12 @@ void trim(std::string &str){
 	rtrim(str);
 }
 
-void ltrim(std::string &str){
+void ltrim(std::string &s){
 	s.erase(s.begin(), std::find_if(s.begin(), s.end(),
 			std::not1(std::ptr_fun<int, int>(std::isspace))));
 }
 
-void rtrim(std::string &str){
+void rtrim(std::string &s){
 	s.erase(std::find_if(s.rbegin(), s.rend(),
 			std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
 }
