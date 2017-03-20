f114c3cb20d37b0076e51e1ede2c015cdf7c3e5b
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-03-19 20:58:38 -0500

    Removed shared_ptr for the gamepad reference in Game.cpp
    This was causing a double corruption of memory
    error.  Which means the destructor for the manager
    reference was being called twice.

--- game_engine/src/Game.cpp
+++ game_engine/src/Game.cpp
@@ -2,6 +2,6 @@
 #include "Manager.h"
 
 int main(){
-	std::shared_ptr<manager> game_manager(manager::get_manager("./assets/xmls/game.xml", "./assets/logs/my_log.txt"));
+	manager * my_man = manager::get_manager("./assets/xmls/game.xml", "./assets/logs/my_log.txt");
 	return 0;
 }
