dd72ff3c8f8fc0268e5e46b33e2c40a4f591d3ac
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-04-02 18:43:48 -0500

    Had to modify some stuff in Interface.cpp

--- game_engine/src/interfaces/Interface.cpp
+++ game_engine/src/interfaces/Interface.cpp
@@ -1,22 +1,28 @@
 #include "Interface.h"
+#include "GUIParser.h"
+#include "Render.h"
+
+#include <iostream>
 
 render * interface::my_renderer = nullptr;
 uint64 interface::ref_counter = 0;
 
-void interface::init(){
+void interface::init(render * the_renderer){
 	// Do something here
-}
-
-interface::interface(render * the_renderer){
-	if(my_renderer == nullptr)
+	if(my_renderer == nullptr){
 		my_renderer = the_renderer;
-	else{
-		if(my_renderer != the_renderer){ // If they are different references.
-			my_renderer = the_renderer;
-		}
 	}
 	ref_counter++;
-	init();
+}
+interface::interface(render * the_renderer, const std::string &document){
+	std::cout << "INTERFACE::RENDER:::::::: " << the_renderer << std::endl;
+	my_renderer = the_renderer;
+	my_parser = new guiparser(document);
+}
+interface::interface(render * the_renderer, const std::vector<std::pair<std::string, std::string> > acts):
+actions(acts){
+	my_parser = nullptr;
+	init(the_renderer);
 }
 
 interface::~interface(){
@@ -24,4 +30,5 @@ interface::~interface(){
 	if(ref_counter == 0)
 		my_renderer = nullptr;
 	actions.clear();
+	delete my_parser;
 }
