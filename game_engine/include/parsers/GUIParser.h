ebe67b90e39092c84bd633371e8baccc83514595
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-04-02 18:42:24 -0500

    Updated GUIParser.h to include newly defined methods.

--- game_engine/include/parsers/GUIParser.h
+++ game_engine/include/parsers/GUIParser.h
@@ -2,16 +2,37 @@
 #define PARSERS_GUIPARSER_H
 
 #include "Parser.h"
+#include <vector>
+#include <string>
+#include <utility>
 
-class guiparser : parser{
+class interface;
+
+class guiparser : public parser{
 	private:
 		// To-Do:	Add helper methods.
+		const static std::string main_tag; // Main tag!!!!
+		const static std::string type; // Object type.
+		
+	//	void parse_resources(interface * my_interface, tinyxml2::XMLElement * resources_tag);
+	//	void parse_defaults(interface * my_interface, tinyxml2::XMLElement * tooltips_tag);
+	//	void parse_widgets(interface * my_interface);
+	//	void parse_locations(interface * my_interface, tinyxml2::XMLElement * locations_tag);
+		
+		void build_window(interface * i, tinyxml2::XMLElement * window_element);
 		
 	public:
 		guiparser(const std::string &file);
 		virtual ~guiparser();
 		
-		void build_gui(interface * interface);
+		virtual std::string get_name();
+		virtual std::string get_type();
+		
+		void build_gui(interface * my_interface);
+		
+		std::vector<std::string> get_fonts();
+		std::vector<std::string> get_schemes();
+		std::vector<std::pair<std::string, std::string> > get_defaults();
 };
 
 #endif
