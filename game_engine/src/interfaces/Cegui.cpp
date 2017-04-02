5010f810b3fa239cf99c436f953e56a2c78cc28c
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-04-02 18:39:56 -0500

    Updated parsers to extend parser class.

--- game_engine/src/interfaces/Cegui.cpp
+++ game_engine/src/interfaces/Cegui.cpp
@@ -1,32 +1,154 @@
 #include "Cegui.h"
 #include "GUIParser.h"
+#include <iostream>
+#include "Render.h"
+
+#include "GameException.h"
+#include "SudoExcept.h"
+
+CEGUI::OgreRenderer * cegui::my_ogre_renderer = nullptr;
+uint cegui::ogre_render_count = 0; // Initialise count for the Ogre Renderer.
 
 void cegui::init(){
-	// Need to initiaize resource locations.
+	if(my_ogre_renderer == nullptr){
+		my_ogre_renderer = &CEGUI::OgreRenderer::bootstrapSystem();
+	}
+	
+	// Grab all schemes specified in XML file.
+	my_schemes = my_parser->get_schemes();
+	if(my_schemes.size() < 1) throw_trace("Need atleast 1 scheme specified.");
+	CEGUI::SchemeManager * sm = CEGUI::SchemeManager::getSingletonPtr();
+	for(uint c = 0; c < my_schemes.size(); c++){
+		sm->createFromFile(my_schemes[c]);
+	}
+	// Grab all fonts specified in XML file.
+	my_fonts = my_parser->get_fonts();
+	if(my_fonts.size() < 1) throw_trace("Need atleast 1 font specified.");
+	CEGUI::FontManager * fm = CEGUI::FontManager::getSingletonPtr();
+	for(uint c = 0; c < my_fonts.size(); c++){
+		fm->createFromFile(my_fonts[c]);
+	}
+	
+	// Need to get system information.
 	my_system = CEGUI::System::getSingletonPtr();
-	my_context = my_system->createGUIContext();
+	// Create GUI context using the system.
+	my_context = &my_system->createGUIContext(my_ogre_renderer->getDefaultRenderTarget());
 	
-	CEGUI::WindowManager * window_manager = CEGUI::WindowManager::getSingletonPtr();
-	my_window = nullptr;
+	// Pull default information from GUI XML file.
+	std::vector<std::pair<std::string, std::string> > default_info = my_parser->get_defaults();
+	bool font_flag = false;
+	bool mouse_flag = false;
+	bool toolt_flag = false;
+	for(uint cx = 0; cx < default_info.size(); cx++){
+		if(!font_flag && default_info[cx].first.compare("font") == 0){
+			my_context->setDefaultFont(default_info[cx].second);
+			font_flag = true;
+		}
+		else if(!mouse_flag && default_info[cx].first.compare("mouse") == 0){
+			my_context->getMouseCursor().setDefaultImage(default_info[cx].second);
+			mouse_flag = true;
+		}
+		else if(!toolt_flag && default_info[cx].first.compare("tooltip") == 0){
+			my_context->setDefaultTooltipType(default_info[cx].second);
+			toolt_flag = true;
+		}
+		if(font_flag && mouse_flag && toolt_flag) break;
+	}
+	if(!font_flag) throw_trace("Need a default font specified.");
+	else if(!mouse_flag) throw_trace("Need a default mouse specified.");
+	else if(!toolt_flag) throw_trace("Need a default tooltip type specified.");
+	else my_renderer->log("Default tooltips setup.");
 	
-	my_parser.build_gui(this);
+	// Create window manager and root window.
+	my_win_manager = CEGUI::WindowManager::getSingletonPtr();
+	my_window = my_win_manager->createWindow("DefaultWindow","root_window");
+	my_context->setRootWindow(my_window);
 }
-void cegui::create_context(){
-	CEGUI
+/*
+void cegui::init(){
+	if(my_ogre_renderer == nullptr){
+		my_ogre_renderer = &CEGUI::OgreRenderer::bootstrapSystem();
+	}
+	CEGUI::SchemeManager::getSingleton().createFromFile("TaharezLook.scheme");
+	CEGUI::FontManager::getSingleton().createFromFile("GreatVibes-16.font");
+	CEGUI::System& sys = CEGUI::System::getSingleton();
+	my_context = &sys.createGUIContext(my_ogre_renderer->getDefaultRenderTarget());
+	my_context->setDefaultFont("GreatVibes-16");
+	my_context->getMouseCursor().setDefaultImage("TaharezLook/MouseArrow");
+	my_context->setDefaultTooltipType("TaharezLook/Tooltip");
+	CEGUI::WindowManager * wmgr = &CEGUI::WindowManager::getSingleton();
+//	my_window = wmgr->loadLayoutFromFile("MyDemo.layout");
+//	my_context->setRootWindow(my_window);
+	CEGUI::Window * my_root = wmgr->createWindow("DefaultWindow","root");
+	CEGUI::System::getSingleton().getDefaultGUIContext().setRootWindow(my_root);
+	CEGUI::FrameWindow * fwnd = static_cast<CEGUI::FrameWindow*>(wmgr->createWindow("TaharezLook/FrameWindow","testWindow"));
+	my_root->addChild(fwnd);
+	// position a quarter of the way in from the top-left of parent.
+	fwnd->setPosition(CEGUI::UVector2( CEGUI::UDim( 0.25f, 0.0f ), CEGUI::UDim( 0.25f, 0.0f ) ) );
+	// set size to be half the size of the parent
+	fwnd->setSize(CEGUI::USize(CEGUI::UDim( 0.5f, 0.0f ),CEGUI::UDim( 0.5f, 0.0f ) ) );
+	fwnd->setText("Hello World");
+}*/
+
+void cegui::destroy_context(){
+	/*
+	my_system->destroy(); // Deallocate system first.
+	my_system = nullptr; // Ensure it references NULL.
+	CEGUI::OgreRenderer::destroy(my_ogre_renderer);
+	my_ogre_renderer = nullptr;
+	*/
+//	CEGUI::OgreRenderer::destroySystem();
 }
-void cegui::destroy_context(){}
 
 cegui::cegui(render * r, const std::string &setup):
-interface(r),my_parser(setup){
-	init();
+interface(r, setup){
+	my_renderer = r;
+	std::cout << "CEGUI::: :::: " << my_renderer << std::endl;
+	my_context = nullptr; // Initialize nonstatic methods to reference NULL
+	my_system = nullptr;
+	my_win_manager = nullptr;
+	my_window = nullptr;
+	init(); // Initialize the the GUI.
+	
+	my_parser->build_gui(this); // Builds widgets.
+	
+//	my_parser->build_gui(this); // Parse the interfaces.
+//	init();
+//	for(std::string resource : my_resources){
+//		my_renderer->load_resource(resource);
+//	}
 }
 cegui::cegui(render * r, std::vector<std::pair<std::string, std::string> > &acts):
-interface(r),actions(acts){}
-cegui::~cegui(){}
-
+interface(r, acts){
+	std::cout << "CEGUI::: :::: " << my_renderer << std::endl;
+	my_context = nullptr;
+	my_system = nullptr;
+	my_window = nullptr;
+	init();
+}
+cegui::~cegui(){
+	ogre_render_count--; // Decrement reference counter
+	if(ogre_render_count == 0){ // Check if it is 0
+		destroy_context(); // If so, then destroy GUI render system.
+	}
+}
+/*
 void cegui::load_level(){}
 void cegui::unload_level(){}
-
+*/
+/*
+void cegui::create_widget(const std::string &type, std::pair<float, float> left, std::pair<float, float> top, std::pair<float, float> right, std::pair<float, float> bottom, const std::string &name){
+	
+	CEGUI::Window * new_widget = CEGUI::WindowManager::getSingleton().createWindow(type, name);
+	
+	my_window->addChild(new_widget);
+	uint widget_id = new_widget->getID(); // get the widget's id.
+	
+	widget_mappings.push_back(std::pair<uint, std::string>(widget_id, name)); // Store the id mapping.
+	new_widget->setPosition(CEGUI::UVector2(left.first, left.second), CEGUI::UVector2(top.first, top.second));
+	new_widget->setSize(CEGUI::UDIM(right.first, right.second), CEGUI::UDIM(bottom.first, bottom.second));
+}
+*/
 void cegui::key_event(const std::string &event){}
 
 void cegui::mouse_move_event(std::vector<int> &abs, std::vector<int> &rel){
@@ -37,7 +159,7 @@ void cegui::mouse_move_event(std::vector<int> &abs, std::vector<int> &rel){
 	my_context->injectMouseMove(rel[0], rel[1]);
 }
 
-void cegui::mouse_press_event(uint8 button, std::vector<int> &abs, std::vector<int> &rel){
+void cegui::mouse_click_event(uint8 button, std::vector<int> &abs, std::vector<int> &rel){
 	CEGUI::MouseButton click_id = CEGUI::NoButton;
 	switch(button){
 		case 1:
@@ -49,7 +171,12 @@ void cegui::mouse_press_event(uint8 button, std::vector<int> &abs, std::vector<i
 		default:
 			click_id = CEGUI::MiddleButton;
 	}
-	my_context->injectMouseButtonDown(click_id);
+	try{
+		my_context->injectMouseButtonDown(click_id);
+	}
+	catch(std::runtime_error &e){
+		throw_trace(std::string(e.what()));
+	}
 }
 
 // 
@@ -65,7 +192,12 @@ void cegui::mouse_release_event(uint8 button, std::vector<int> &abs, std::vector
 		default:
 			click_id = CEGUI::MiddleButton;
 	}
-	my_context->injectMouseButtonUp(click_id);
+	try{
+		my_context->injectMouseButtonUp(click_id);
+	}
+	catch(std::runtime_error &e){
+		throw_trace(std::string(e.what()));
+	}
 }
 
 // Doing nothing with this function.
@@ -73,56 +205,103 @@ void cegui::mouse_wheel_event(std::vector<int> &abs, std::vector<int> &rel){
 	// implemented in mouse_move_event.
 }
 
-void set_resource_loc(resource_t &type, const std::string &name, const std::string &loc){
-	CEGUI::DefaultResourceProvider * my_provider = static_cast<CEGUI::DefaultResourceProvider*>(CEGUI::System::getSingletion().getResourceProvider());
-	my_provider->setResourceGroupDirectory(rsrc, loc); // set resource directory to be this.
-	switch(type){ // Determine type of resource we are adjusting location for.
-		case resource_t::DESIGN:
-			CEGUI::WidgetLookManager::setDefaultResourceGroup(name);
-			break;
-		case resource_t::FONT:
-			CEGUI::Font::setDefaultResourceGroup(name);
-			break;
-		case resource_t::IMAGESET:
-			CEGUI::ImageManager::setImagesetDefaultResourceGroup(name);
-			break;
-		case resource_t::LAYOUT:
-			CEGUI::WindowManager::setDefaultResourceGroup(name);
-			break;
-		case resource_t::SCHEME:
-			CEGUI::Scheme::setDefaultResourceGroup(name);
-			break;
-		default:
-			CEGUI::ScriptModule::setDefaultResourceGroup(name);
-	}
+void cegui::set_resource_loc(const std::string &name, const std::string &loc){
+//	CEGUI::DefaultResourceProvider * my_provider = static_cast<CEGUI::DefaultResourceProvider*>(my_system->getResourceProvider());
+//	my_provider->setResourceGroupDirectory(rsrc, loc); // set resource directory to be this.
+//	try{
+//		my_renderer->add_resource_location(loc, name); // Since we are using the Ogre renderer, we can use Ogre's ResourceGroupManager.
+//		my_renderer->load_resource(name); // GUI may not load without this.
+//	}
+//	catch(game_error &e){
+//		throw_trace(std::string(e.what()));
+//	}
+//	my_resources.push_back(name);
 }
-		
-// Setter functions.
-void set_resource(resource_t &type, const std::string &file){
-	switch(type){ // Determine type of resource we are adjusting location for.
-		case resource_t::DESIGN:
-		//	
-			break;
-		case resource_t::FONT:
-		//	CEGUI::Font::setDefaultResourceGroup(name);
-			my_context->setDefaultFont(file);
-			break;
-		case resource_t::IMAGESET:
-		//	CEGUI::ImageManager::setImagesetDefaultResourceGroup(name);
-			break;
-		case resource_t::LAYOUT:
-		//	CEGUI::WindowManager::setDefaultResourceGroup(name);
-			CEGUI::WindowManager * window_manager = CEGUI::WindowManager::getSingletonPtr();
-			my_window = window_manager->loadLayoutFromFile(file);
-			break;
-		case resource_t::SCHEME:
-		//	CEGUI::Scheme::setDefaultResourceGroup(name);
+void cegui::create_resource(const std::string &type, const std::string &file){
+	try{
+		if(type.compare("font") == 0){
+			CEGUI::FontManager::getSingleton().createFromFile(file);
+			my_fonts.push_back(file);
+			if(my_fonts.size() == 1){
+				set_default(type, file);
+			}
+		}
+		else if(type.compare("scheme") == 0){
 			CEGUI::SchemeManager::getSingleton().createFromFile(file);
-			break;
-		default:
-		//	CEGUI::ScriptModule::setDefaultResourceGroup(name);
+		}
+		else if(type.compare("layout") == 0){
+			CEGUI::Window * window = my_win_manager->loadLayoutFromFile(file);
+			widgets.push_back(window);
+			if(widgets.size() == 1)
+				my_context->setRootWindow(window);
+		}
+		else if(type.compare("imageset") == 0){
+			CEGUI::ImageManager::getSingleton().loadImageset(file, "my_images");
+		}
+		else{
+			std::cout << "Type not initialized." << std::endl;
+		}
+	}
+	catch(std::runtime_error &e){
+		throw_trace(std::string(e.what()));
 	}
 }
-void apply_script(const std::string &widget, const std::string &script){
-	
+void cegui::set_default(const std::string &type, const std::string &file){
+	int d_loc = file.find_last_of('.'); // Need to trim the extension off of the filename.
+	std::string str(file);// Copy to modify.
+	str = str.substr(0, d_loc);
+	int bs_loc = str.find_last_of('\\');
+	if(bs_loc != std::string::npos) // Check if there is a backslash in the filename.
+		str = str.substr(bs_loc+1); // If so, then trim it.
+	int fs_loc = str.find_last_of('/');
+	if(fs_loc != std::string::npos){ // Check if there is a forwardslash in the filename.
+		str = str.substr(fs_loc+1); // If so, then trim it.
+	}
+	set_default_component(type, str);
+}
+// Setter functions.
+void cegui::load_resource(const std::string &name){
+	try{
+		my_renderer->load_resource(name);
+	}
+	catch(game_error &e){
+		throw_trace(std::string(e.what()));
+	}
+}
+void cegui::set_default_component(const std::string &component, const std::string &value){
+//	std::cout << "\n\n\tcomponent: " << component << "\n\n" << std::endl;
+	try{
+		if(component.compare("font") == 0){
+			my_context->setDefaultFont(value);
+		}
+		else if(component.compare("mouse") == 0){
+			my_context->getMouseCursor().setDefaultImage(value);
+		}
+		else if(component.compare("tooltip") == 0){
+			my_context->setDefaultTooltipType(value);
+		}
+		else{
+			my_renderer->log("Undefined component.");
+		}
+	}
+	catch(game_error &e){
+		throw_trace(std::string(e.what()));
+	}
+	catch(std::runtime_error &e){
+		throw_trace(std::string(e.what()));
+	}
+}
+void cegui::apply_script(const std::string &widget, const std::string &script){
+	uint id;
+	bool flag = false;
+	for(std::pair<uint, std::string> mapping : widget_mappings){
+		if(mapping.second.compare(widget) == 0){
+			id = mapping.first;
+			flag = true;
+			break;
+		}
+	}
+	if(flag){ // do stuff
+		std::cout << "id mapped" << std::endl;
+	}
 }
---
+++ game_engine/src/parsers/GUIParser.cpp
@@ -0,0 +1,127 @@
+#include "GUIParser.h"
+#include "Interface.h"
+#include "GameException.h"
+#include "SudoExcept.h"
+
+const std::string guiparser::main_tag = "gui";
+
+guiparser::guiparser(const std::string &file):
+parser(file, main_tag){}
+
+guiparser::~guiparser(){}
+
+void guiparser::build_gui(interface * my_interface){
+	
+	tinyxml2::XMLElement * tag = get_main_element();
+	tinyxml2::XMLElement * widgets_tag = tag->FirstChildElement("widgets");
+	for(tinyxml2::XMLElement * window_tag = widgets_tag->FirstChildElement("window"); window_tag != nullptr; window_tag = window_tag->NextSiblingElement("window")){
+		std::string child_name = build_window(my_interface, window_tag);
+		my_interface->add_root_child(child_name);
+	}
+}
+
+std::string guiparser::build_window(interface * my_interface, tinyxml2::XMLElement * my_window){
+	const char * name_cstr = my_window->Attribute("name"); // Grab the name, type, and look attributes to build the window
+	if(name_cstr == nullptr) throw_trace("<window> requires a 'name' attribute.");	// Check if they exist, if not, then throw error.
+	const char * type_cstr = my_window->Attribute("type");
+	if(type_cstr == nullptr) throw_trace("<window> requires a 'type' attribute.");
+	const char * look_cstr = my_window->Attribute("look");
+	if(look_cstr == nullptr) throw_trace("<window> requires a 'look' attribute.");
+	
+	tinyxml2::XMLElement * loc_tag = my_window->FirstChildElement("location"); // Grab <location> and <area>
+	if(loc_tag == nullptr) throw_trace("<location> must be present within <window> tree."); // Check if they exist, if not, throw error.
+	tinyxml2::XMLElement * area_tag = my_window->FirstChildElement("area");
+	if(area_tag == nullptr) throw_trace("<area> must be present within <window> tree.");
+	
+	const char * loc_abs = loc_tag->Attribute("absolute"); // Grab <location>'s and <area>'s absolute and relative attributes
+	if(loc_abs == nullptr) throw_trace("<location> needs an 'absolute' attribute."); // Check if they exist, if not, then throw error.
+	const char * loc_rel = loc_tag->Attribute("relative");
+	if(loc_abs == nullptr) throw_trace("<location> needs an 'relative' attribute.");
+	const char * area_abs = area_tag->Attribute("absolute");
+	if(loc_abs == nullptr) throw_trace("<area> needs an 'absolute' attribute.");
+	const char * area_rel = area_tag->Attribute("relative");
+	if(loc_abs == nullptr) throw_trace("<area> needs an 'relative' attribute.");
+	
+	std::string name_str(name_cstr); // name, type, and look exist, so convert them to std::string and trim whitespace off.
+	trim(name_str);
+	std::string type_str(type_cstr);
+	trim(type_str);
+	std::string look_str(look_cstr);
+	trim(look_str);
+	my_interface->create_widget(name_str, type_str, look_str); // Create the widget.
+	
+	std::string lastr(loc_abs); // <location>'s and <area>'s relative and absolute attributes exist, so convert them to vector<float>s
+	trim(lastr);
+	std::vector<float> location_absolutes = parse_fvector(lastr);
+	std::string lrstr(loc_rel);
+	trim(lrstr);
+	std::vector<float> location_relatives = parse_fvector(lrstr);
+	std::string aastr(area_abs);
+	trim(aastr);
+	std::vector<float> area_absolutes = parse_fvector(aastr);
+	std::string arstr(area_rel);
+	trim(arstr);
+	std::vector<float> area_relatives = parse_fvector(arstr);
+	
+	my_interface->set_position(name_str, location_absolutes, location_relatives); // Set the position and area for the widget.
+	my_interface->set_area(name_str, area_absolutes, area_relatives);
+	
+	for(tinyxml2::XMLElement * window_tag = my_window->FirstChildElement("window"); window_tag != nullptr; window_tag = window_tag->NextSiblingElement("window")){ // Recursively create this widget's children.
+		std::string child_name = build_window(my_interface, window_tag);
+		my_interface->add_child(name_str, child_name);
+	}
+	
+	tinyxml2::XMLElement * text_tag = my_window->FirstChildElement("text");
+	if(text_tag != nullptr){
+		std::string text_str(text_tag->GetText());
+		my_interface->set_text(text_str);
+	}
+	return name_str;
+}
+
+
+
+
+std::vector<std::string> guiparser::get_fonts(){
+	std::vector<std::string> fonts;
+	
+	tinyxml2::XMLElement * tag = get_main_element();
+	tinyxml2::XMLElement * fonts_tag = tag->FirstChildElement("fonts");
+	if(fonts_tag == nullptr) throw_trace("<fonts> is needed in XML file.");
+	for(tinyxml2::XMLElement * font_tag = fonts_tag->FirstChildElement("font"); font_tag != nullptr; font_tag = font_tag->NextSiblingElement("font")){
+		const char * file_cstr = font_tag->Attribute("file");
+		std::string file_str(file_cstr);
+		fonts.push_back(file_str);
+	}
+	return fonts;
+}
+std::vector<std::string> guiparser::get_schemes(){
+	std::vector<std::string> schemes; // Vector we wish to return.
+	
+	tinyxml2::XMLElement * tag = get_main_element();
+	tinyxml2::XMLElement * schemes_tag = tag->FirstChildElement("schemes");
+	if(schemes_tag == nullptr) throw_trace("<schemes> is needed in XML file.");
+	// Loop over all <scheme> tags. Pull the contents of the 'file' attribute and store it in the schemes vector.
+	for(tinyxml2::XMLElement * scheme_tag = schemes_tag->FirstChildElement("scheme"); scheme_tag != nullptr; scheme_tag = scheme_tag->NextSiblingElement("scheme")){
+		const char * file_cstr = scheme_tag->Attribute("file");
+		std::string file_str(file_cstr);
+		schemes.push_back(file_str);
+	}
+	
+	return schemes;
+}
+std::vector<std::pair<std::string, std::string> > guiparser::get_defaults(){
+	std::vector<std::pair<std::string, std::string> > defaults;
+	
+	tinyxml2::XMLElement * tag = get_main_element();
+	tinyxml2::XMLElement * defaults_tag = tag->FirstChildElement("defaults");
+	if(defaults_tag == nullptr) throw_trace("<defaults> is needed in XML file.");
+	
+	for(tinyxml2::XMLElement * default_tag = defaults_tag->FirstChildElement("default"); default_tag != nullptr; default_tag = default_tag->NextSiblingElement("default")){
+		const char * type_cstr = default_tag->Attribute("type");
+		const char * name_cstr = default_tag->Attribute("name");
+		std::pair<std::string, std::string> new_pair(std::string(type_cstr), std::string(name_cstr));
+		defaults.push_back(new_pair);
+	}
+	return defaults;
+}
