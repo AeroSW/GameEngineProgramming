029ba9a8e039dc6581901e213f253c961415c9cf
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-04-08 02:39:02 -0500

    Changed cegui_resource to hold a vector of strings over a single string.

--- game_engine/include/interfaces/CeguiWindowResource.h
+++ game_engine/include/interfaces/CeguiWindowResource.h
@@ -6,7 +6,7 @@
 struct cegui_resource{
 	std::string my_name;
 	std::string my_type;
-	std::string my_script;
+	std::vector<std::string> my_script;
 	CEGUI::Window * my_window;
 	cegui_resource(); // Identity Constructor
 	cegui_resource(const std::string &name, CEGUI::Window * window, const std::string &my_type="Default Window", const std::string &my_script=""); // Regular constructor
