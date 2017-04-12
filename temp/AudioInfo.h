40440adb42748a0e0e7548beb09fe1aac433cc01
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-04-12 10:00:50 -0500

    Ignoring files no longer part of project as well as files with .mp3 ext

--- temp/AudioInfo.h
+++
@@ -1,8 +0,0 @@
-#ifndef AUDIO_AUDIO_INFO_H
-#define AUDIO_AUDIO_INFO_H
-
-#include "UnsignedTypes.h"
-
-struct audio_info{};
-
-#endif
--- temp/InterfaceArguments.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERFACES_INTERFACE_ARGUMENTS_H
-#define INTERFACES_INTERFACE_ARGUMENTS_H
-
-class interface_args{
-	public:
-		virtual ~interface(){}
-};
-
-#endif
