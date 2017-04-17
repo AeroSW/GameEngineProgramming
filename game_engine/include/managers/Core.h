7a6f1955c247fe843dd36caef9eaf046ca6a903a
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-04-17 01:08:16 -0500

    Defined and implemented core class in Core.h/.cpp
    Defined core class in Core.h and implemented it in Core.cpp.
    
    This class will be replacing the manager class defined earlier in the project.
    It is built with the Mediator Design Pattern in mind.  The implementation of
    manager contained dependency issues with render class.  The dependencies
    were the scene class and level class.  I designed the class core with the intention
    to eliminate these dependencies as well as to enforce myself to document the
    methods which are handling the message passing and minimize the number
    of methods needing to be defined, since, script manager has now been created
    and many of the predefined methods can now be moved into script calls.

---
+++ game_engine/include/managers/Core.h
@@ -0,0 +1,408 @@
+#ifndef MANAGERS_CORE_H
+#define MANAGERS_CORE_H
+
+// STD Libraries
+#include <string>
+#include <vector>
+// Personal Libraries
+#include "UnsignedTypes.h"
+#include "MyEnums.h"
+
+// Forward Declarations
+class audio;
+class input;
+//class level;
+class logger; // Going to be static methods.
+class render;
+class scripter;
+class gameparser;
+
+//enum input_t{KEYBOARD=1,MOUSE,DUALSHOCK4,XBOX1};
+
+class core{
+	private:
+		static core * core_manager;
+		static audio * audio_manager;
+	//	static level * level_manager; // Planning on making this a sub-type to render_manager
+		static scripter * script_manager;
+		static render * render_manager;
+		
+		static gameparser * my_parser;
+		
+		static bool input_flag;
+		
+	//	enum gamepad_t{DS4=1,XB1,NONE};
+		std::vector<input*> input_managers;
+	//	gamepad_t gamepad_type;
+
+		virtual void core_init(const std::string &dox);
+		virtual void render_init(const std::string &dox);
+		virtual void audio_init(const std::string &dox);
+		virtual void inputs_init(input_t &type, const std::string &dox);
+		virtual void scripts_init(const std::string &dox);
+
+	protected:
+		/*!
+		 *	\brief The constructor for the game engine's core.
+		 *	\details The constructor which creates the game engine based off the given XML
+		 *	file.
+		 *	\exception game_error A game_error is generated when certain criteria are not met for the
+		 *	game engine's creation.
+		 */
+		core(const std::string &my_dox);
+		/*!
+		 *	\brief The destructor for the game engine.
+		 *	\details The destructor which calls the destructors for the fundamental components
+		 *	in this game engine if they exist.
+		 */
+		virtual ~core();
+
+	public:
+		/*!
+		 *	\brief A function which will initialize the game engine.
+		 *	\details A function which initializes the game engine using the given xml
+		 *	document name as an argument.
+		 *
+		 *	\param dox A name to the XML file which holds the names of the other documents
+		 *	needed to finish constructing the game engine.
+		 *
+		 *	\return A boolean value indicating whether the game engine was successfully
+		 *	constructed or not.
+		 */
+		static bool initialize(const std::string &dox);
+		/*!
+		 *	\brief A function which will terminate the game engine.
+		 *	\details A function which will call the core's destructor to terminate the
+		 *	game engine's processes.
+		 */
+		static bool destroy();
+		/*!
+		 *	\brief A function which will return a pointer reference to the core manager.
+		 *	\details A function which returns a pointer to the core manager singleton object.
+		 *
+		 *	\param void
+		 *	\return A pointer to the reference of the core manager.
+		 *	\exception game_error A game_error will be generated if the singleton object
+		 *	does not exist.
+		 */
+		static core * get_singleton_ptr();
+		
+		/*************************************************//***********
+		 *		Manager Creation Functions.
+		 *************************************************//**********/
+		
+		/*!
+		 *	\brief Function to construct the render manager.
+		 *	\details A function which will construct the render manager object using
+		 *	the provided XML document.
+		 *
+		 *	\param render_dox Path/Name of the XML document which will construct the Render Manager.
+		 *	\return void
+		 *	\exception game_error The game_error exception is generated whenever a failure to
+		 *	initialize the render manager occurs.
+		 */
+		virtual void create_render(const std::string &render_dox);
+		/*!
+		 *	\brief A function which will create an input of the given type.
+		 *	\details A function which will create the input manager for the given type of
+		 *	input.
+		 *
+		 *	\param type An input_t enum describing the type of input manager to create.
+		 *	\return void
+		 *	\exception game_error A game_error is generated if the input manager is failed
+		 *	to be created or already exists.
+		 */
+		virtual void create_input(input_t &type, const std::string &input_dox);
+	//	virtual void create_level(const std::string &lvl_dox);
+		/*!
+		 *	\brief A function which initializes the log manager.
+		 *	\details A function which initializes the log manager to store log information
+		 *	into the given log text document.
+		 *
+		 *	\param log_dox Path and name for the document where the logs for the core
+		 *	will be stored.
+		 *	\return void
+		 */
+		virtual void create_logger(const std::string &log_dox);
+		/*!
+		 *	\brief A function which will initialize the script manager.
+		 *	\details A function which will initialize the script manager with the given XML
+		 *	documentation.
+		 *
+		 *	\param script_dox The document which is needed to initialize the script manager.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if the script manager is failed to be
+		 *	initialized.
+		 */
+		virtual void create_scripter(const std::string &script_dox);
+		/*!
+		 *	\brief A function which will initialize the audio manager with the given document.
+		 *	\details A function which will initialize the audio manager with the given XML document.
+		 *
+		 *	\param audio_dox An XML document containing the information needed for the script manager
+		 *	to be initialized.
+		 *	\return void
+		 *	\exception game_error A game_error is generated when the audio manager is not initialized
+		 *	properly.
+		 */
+		virtual void create_audio(const std::string &audio_dox);
+	//	virtual void create_level(const std::string &level_dox);
+		
+		/***************************************************//******************
+		 *			Window Functions
+		 ***************************************************//*****************/
+		 
+		/*!
+		 *	\brief A function which returns the height of the window in pixels.
+		 *	\details A function which returns the height of the window in pixels.
+		 *
+		 *	\param void
+		 *	\return Unsigned 32-bit integer.
+		 */
+		virtual uint32 get_window_height();
+		/*!
+		 *	\brief A function which returns the width of the window in pixels.
+		 *	\details A function which returns the width of the window in pixels.
+		 *
+		 *	\param void
+		 *	\return Unsigned 32-bit integer.
+		 */
+		virtual uint32 get_window_width();
+		/*!
+		 *	\brief A function which returns the handler for the window.
+		 *	\details A function which returns the handler for the window.
+		 *
+		 *	\param void
+		 *	\return Unsigned 32-bit integer.
+		 */
+		virtual uint32 get_window_handler();
+		
+		/*!
+		 *	\brief A function which returns the name of the current level.
+		 *	\details A function which returns the name of the current level.
+		 *
+		 *	\param void
+		 *	\return C++ Standard String object containing the level's name.
+		 */
+		virtual std::string level_name();
+		
+		/******************************************************************//*****
+		 *				Manager Message Passing Routines
+		 ***************************//*******************************************/
+		 
+		 // Script Message Passing Methods
+		 
+		/*!
+		 *	\brief A function which passes the method to execute a script to the script manager.
+		 *	\details A function which passes the name of a script to be executed and its
+		 *	arguments to the script manager.
+		 *
+		 *	\param script The path and name of the script which will be executed be the script
+		 *	manager.
+		 *	\param script_arguments A vector of arguments which the script will need to execute.
+		 *	(Note, the vector of script arguments can be empty if the script has no arguments.)
+		 *	\return void
+		 */
+		virtual void call_script(const std::string &script, std::vector<std::string> &script_arguments);
+		
+		// Audio Message Passing Methods
+		
+		/*!
+		 *	\brief A function which will start the audio queue.
+		 *	\details A function which will inform the audio manager to play all songs which have
+		 *	been queued up until this point and until the queue is empty.
+		 *
+		 *	\param void
+		 *	\return void
+		 */
+		virtual void play_audio();
+		/*!
+		 *	\brief A function which will stop audio from playing.
+		 *	\details A function which will tell the audio manager to stop playing audio and
+		 *	empty the current queued list.
+		 *
+		 *	\param void
+		 *	\return void
+		 */
+		virtual void stop_audio();
+		/*!
+		 *	\brief A function which will add the given audio file onto the audio manager's queue.
+		 *	\details A function which informs the audio manager to add the given mp3 file onto its
+		 *	play queue.
+		 *
+		 *	\param audio_name The path and name for the MP3 audio file which is to be placed onto
+		 *	the queue.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if the audio file does not exist.
+		 */
+		virtual void queue_audio(const std::string &audio_name);
+		
+		// Render Message Passing Methods
+		
+		/*!
+		 *	\brief Loads level with given name.
+		 *	\details This function passes a message to the render manager to load a level with the
+		 *	given name.
+		 *
+		 *	\param level_name The name of the level to be loaded.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if a level with the given name
+		 *	does not exist.
+		 */
+		virtual void switch_level(const std::string &level_name);
+		/*!
+		 *	\brief Translates the given object by the given adjustments.
+		 *	\details Translates the object with name 'object_name' by x, y, and z units from its local
+		 *	position.
+		 *
+		 *	\param object_name The name of the object to be moved.
+		 *	\param x The amount of units along the x axis for the object to be moved.
+		 *	\param y The amount of units along the y axis for the object to be moved.
+		 *	\param z The amount of units along the z axis for the object to be moved.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if an object with the given name does
+		 *	not exist.
+		 */
+		virtual void move_object(const std::string &object_name, float x, float y, float z);
+		/*!
+		 *	\brief Rotates the given object about the given axis(global or local) by w radians.
+		 *	\details Informs the render manager that the object with 'object_name' needs to be
+		 *	rotated by 'w' radians along its local/global 'axis'.
+		 *
+		 *	\param object_name The name of the object being rotated.
+		 *	\param axis An enum value describing which axis to rotate about.
+		 *	\param type An enum value describing if the rotation is about the global or local axis.
+		 *	\param w The angle in radians which the object will be rotated.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if an object with the given name does
+		 *	not exist.
+		 */
+		virtual void rotate_object(const std::string &object_name, axis_t &axis, rotation_t &type, float w);
+		/*!
+		 *	\brief A function which will scale the requested object's dimensions.
+		 *	\details A function which will tell the render manager to scale the object with the
+		 *	given name by the provided values.  The object's local x will be scaled by the given
+		 *	x value.  The same will occur with the given y and z values.
+		 *
+		 *	\param object_name The name of the object which is being scaled.
+		 *	\param x The value which the object's local x will be scaled by.
+		 *	\param y The value which the object's local y will be scaled by.
+		 *	\param z The value which the object's local z will be scaled by.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if the object does not exist.
+		 */
+		virtual void scale_object(const std::string &object_name, float x, float y, float z);
+		/*!
+		 *	\brief A function which reveals a hidden object with the given name.
+		 *	\details A function which tells the render manager to reveal an object
+		 *	with the name 'object_name'.
+		 *
+		 *	\param object_name The name of the object to be revealed.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if the object does not exist.
+		 */
+		virtual void show_object(const std::string &object_name);
+		/*!
+		 *	\brief A function which hides an object with the given name.
+		 *	\details A function which tells the render manager to hide an object
+		 *	with the name 'object_name'.
+		 *
+		 *	\param object_name The name of the object to be hidden.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if the object does not exist.
+		 */
+		virtual void hide_object(const std::string &object_name);
+		
+		// Input Methods
+		
+		/*!
+		 *	\brief A function which simulates the described key being pressed.
+		 *	\details A function which informs render manager of a key being pressed by giving it
+		 *	the key's ascii value.
+		 *
+		 *	\param key The ascii character representation of the key being pressed.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if the given key is not supported
+		 *	by the game engine.
+		 */
+		virtual void key_pressed(char &key);
+		/*!
+		 *	\brief A function which simulates the described key being released.
+		 *	\details A function which informs render manager of a key being released by giving it
+		 *	the key's ascii value.
+		 *
+		 *	\param key The ascii character representation of the key being released.
+		 *	\return void
+		 *	\exception game_error A game_error will be generated if the given key is not supported
+		 *	by the game engine.
+		 */
+		virtual void key_released(char &key);
+		/*!
+		 *	\brief A function which simulates a mouse button being clicked at the given location.
+		 *	\details A function which simulates a mouse button being pressed at the absolute and
+		 *	relative positions given.  This function passes the values given to it to the render
+		 *	manager.
+		 *
+		 *	\param button The button on the mouse which was pressed.
+		 *	\param abs_vals The absolute coordinates for the mouse.
+		 *	\param rel_vals The relative coordinates for the mouse.
+		 *	\return void
+		 *	\exception game_error The action caused by the simulated mouse events does not exist.
+		 */
+		virtual void mbutton_pressed(uint8 button, std::vector<int> &abs_vals, std::string<int> &rel_vals);
+		/*!
+		 *	\brief A function which simulates a mouse button being released at the given location.
+		 *	\details A function which simulates a mouse button being released at the absolute and
+		 *	relative positions given.  This function passes the values given to it to the render
+		 *	manager.
+		 *
+		 *	\param button The button on the mouse which was released.
+		 *	\param abs_vals The absolute coordinates for the mouse.
+		 *	\param rel_vals The relative coordinates for the mouse.
+		 *	\return void
+		 *	\exception game_error The action caused by the simulated mouse events does not exist.
+		 */
+		virtual void mbutton_released(uint8 button, std::vector<int> &abs_vals, std::string<int> &rel_vals);
+		/*!
+		 *	\brief A function which simulates the mouse's movements.
+		 *	\details A function which simulates the movement of a computer mouse.  It passes
+		 *	the given position values onto the render manager.
+		 *
+		 *	\param button The button on the mouse which was released.
+		 *	\param abs_vals The absolute coordinates for the mouse.
+		 *	\param rel_vals The relative coordinates for the mouse.
+		 *	\return void
+		 *	\exception game_error The action caused by the simulated mouse events does not exist.
+		 */
+		virtual void mouse_moved(std::vector<int> &abs_vals, std::string<int> &rel_vals);
+		
+		/*********************************************//*****************
+		 *		Listener Message Passing Routines
+		 ********************************************//*****************/
+		
+		// Audio Listener
+		/*!
+		 *	\brief A function which checks whether the audio should still be playing or not.
+		 *	\details A function which asks the audio manager to check if the current audio
+		 *	has ended or not if it is playing.
+		 *
+		 *	\param timestep The current timestep in the render process.
+		 *	\return void
+		 */
+		virtual void update_audio(float timestep);
+		// Input Listener
+		/*!
+		 *	\brief A function which polls all inputs for changes in their state.
+		 *	\details A function which will ask each input to process a change in state if
+		 *	an event occurred.
+		 *
+		 *	\param timestep The current timestep in the render process.
+		 *	\return void
+		 */
+		virtual void poll_inputs(float timestep);
+		
+		// Initialization of game
+		virtual void start_engine();
+};
+
+#endif
