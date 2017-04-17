3eb5390ae0c8601680a4219dd1eb8e0b42a572c3
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-04-17 01:09:29 -0500

    Created a header file to contain global enums for my game engine.

---
+++ game_engine/include/MyEnums.h
@@ -0,0 +1,9 @@
+#ifndef MYENUMS_H
+#define MYENUMS_H
+
+enum input_t{KEYBOARD=1,MOUSE,DUALSHOCK4,XBOX1};
+enum axis_t{X=0,Y,Z};
+enum rotation_t{GLOBAL=1,LOCAL};
+enum object_t{CAMERA=1,ENTITY,LIGHT,NODE};
+
+#endif
