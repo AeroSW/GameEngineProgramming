f42d90ba7aaa3f8ecabeaf2752dc7b91f9295bf4
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-04-23 02:42:03 -0500

    Added enum for light types

--- game_engine/include/MyEnums.h
+++ game_engine/include/MyEnums.h
@@ -5,5 +5,6 @@ enum input_t{KEYBOARD=1,MOUSE,DUALSHOCK4,XBOX1};
 enum axis_t{X=0,Y,Z};
 enum rotation_t{GLOBAL=1,LOCAL};
 enum object_t{CAMERA=1,ENTITY,LIGHT,NODE};
+enum light_t{POINT=1,SPOTLIGHT,DIRECTIONAL};
 
 #endif
