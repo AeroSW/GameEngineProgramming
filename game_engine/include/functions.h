e636fecffbda09dcd9cc82ea12ce5d6670c47c7c
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-05-02 23:48:13 -0500

    Added functions to namespace asw.  Removed vector<specific> functions.
    Added all utility functions used in game to namespace asw.
    
    Removed parse_vector and parse_fvector.
    Replaced the above functions with a templated stringToVector function.
    Added templated stringToType helper function to utility functions.

--- game_engine/include/functions.h
+++ game_engine/include/functions.h
@@ -1,12 +1,36 @@
 // Functions.h
 #include <vector>
 #include <string>
+#include <sstream>
 
-std::vector<double> parse_vector(const std::string &vect_str);
-std::vector<float> parse_fvector(const std::string &vect_str);
-
-void trim(std::string &str);
-void ltrim(std::string &str);
-void rtrim(std::string &str);
-
-std::wstring string2wstring(const std::string &str);
+namespace asw{
+	template<class T>
+	T stringToType(const std::string &str){
+		std::istringstream ss(str);
+		T val;
+		ss >> val;
+		return val;
+	}
+	
+	template<class T>
+	std::vector<T> stringToVector(const std::string &vect_str, char delim){
+	//	static char delim = ',';
+		std::stringstream ss;
+		ss.str(vect_str);
+		std::string buffer;
+		std::vector<T> value;
+		while (std::getline(ss, buffer, delim)) {
+			T val = stringToType<T>(buffer);
+			value.push_back(val); // Push value back.
+		}
+		return value;
+	}
+	
+//	std::vector<float> parse_fvector(const std::string &vect_str);
+	
+	void trim(std::string &str);
+	void leftTrim(std::string &str);
+	void rightTrim(std::string &str);
+	
+	std::wstring stringToWideString(const std::string &str);
+}
--- game_engine/src/functions.cpp
+++ game_engine/src/functions.cpp
@@ -1,54 +1,29 @@
 // Functions.cpp
-#include "Functions.h"
+#include "include/functions.h"
 
 #include <algorithm>
 #include <functional>
 #include <cctype>
 #include <locale>
-#include <sstream>
 
 #include <codecvt>
 
-std::vector<double> parse_vector(const std::string &vect_str){
-	static char delim = ',';
-	std::stringstream ss;
-	ss.str(vect_str);
-	std::string buffer;
-	std::vector<double> value;
-	while (std::getline(ss, buffer, delim)) {
-		value.push_back(std::stod(buffer));
-	}
-	return value;
+void asw::trim(std::string &str){
+	leftTrim(str);
+	rightTrim(str);
 }
 
-std::vector<float> parse_fvector(const std::string &vect_str){
-	static char delim = ',';
-	std::stringstream ss;
-	ss.str(vect_str);
-	std::string buffer;
-	std::vector<float> value;
-	while (std::getline(ss, buffer, delim)) {
-		value.push_back(std::stof(buffer));
-	}
-	return value;
-}
-
-void trim(std::string &str){
-	ltrim(str);
-	rtrim(str);
-}
-
-void ltrim(std::string &s){
+void asw::leftTrim(std::string &s){
 	s.erase(s.begin(), std::find_if(s.begin(), s.end(),
 			std::not1(std::ptr_fun<int, int>(std::isspace))));
 }
 
-void rtrim(std::string &s){
+void asw::rightTrim(std::string &s){
 	s.erase(std::find_if(s.rbegin(), s.rend(),
 			std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
 }
 
-std::wstring string2wstring(const std::string &str){
+std::wstring asw::stringToWideString(const std::string &str){
 	typedef std::codecvt_utf8<wchar_t> convert_type;
 	std::wstring_convert<convert_type, wchar_t> converter;
 	return converter.from_bytes(str);
