e12ab328cee35be5888de41b547556cc2bcb72cb
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-05-03 12:27:26 -0500

    Removed unused file, added objectstruct to asw.

--- game_engine/include/physics/ObjectStruct.h
+++ game_engine/include/physics/ObjectStruct.h
@@ -1,21 +1,23 @@
-#ifndef PHYSICS_OBJECT_STRUCT_H
-#define PHYSICS_OBJECT_STRUCT_H
+#ifndef ASW_INCLUDE_PHYSICS_OBJECTSTRUCT_H
+#define ASW_INCLUDE_PHYSICS_OBJECTSTRUCT_H
 
 #include <string>
 #include <vector>
 
-enum shape_t{BOX = 1, CONE, CYLINDER, CYLINDER_X, PLANE, SPHERE, SQUARE};
-enum body_t{GHOST = 0, RIGID, SOFT};
-
-struct object_struct{
-	std::vector<float> basis;
-	std::vector<float> inertia;
-	std::vector<float> origin;
-	float mass;
-	shape_t shape;
-//	body_t body;
-	std::string name;
-	std::vector<float> my_args;
-};
+namespace asw{
+	enum shape_t{BOX = 1, CONE, CYLINDER, CYLINDER_X, PLANE, SPHERE, SQUARE};
+	enum body_t{GHOST = 0, RIGID, SOFT};
+	
+	struct object_struct{
+		std::vector<float> basis;
+		std::vector<float> inertia;
+		std::vector<float> origin;
+		float mass;
+		shape_t shape;
+	//	body_t body;
+		std::string name;
+		std::vector<float> my_args;
+	};
+}
 
 #endif
