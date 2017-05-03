ca62fb28c0a11b68c7bd537c41e6a2b560563830
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-05-02 23:48:38 -0500

    Added class GameError to asw namespace.

--- game_engine/src/game_exception.cpp
+++ game_engine/src/game_exception.cpp
@@ -1,18 +1,18 @@
-#include "GameException.h"
+#include "include/game_exception.h"
 
-#include <iostream>
+//#include <iostream>
 
-GameError::GameError(const std::string &what_msg, const std::string &file, uint32 line):
+asw::GameError::GameError(const std::string &what_msg, const std::string &file, uint32 line):
 runtime_error(what_msg){
 	line_number = line;
 	file_name = file;
 }
 
-GameError::~GameError(){}
+asw::GameError::~GameError(){}
 
-const char* GameError::what(){
+const char* asw::GameError::what(){
 //	std::string full_msg = std::string(runtime_error::what()) + " in class " + class_name + " at line " + std::to_string(line_number);
 	std::string msg = std::string(runtime_error::what()) + "\n\t" + file_name + ":" + std::to_string(line_number);
-	std::cout << msg << std::endl;
+//	std::cout << msg << std::endl;
 	return msg.c_str();
 }
