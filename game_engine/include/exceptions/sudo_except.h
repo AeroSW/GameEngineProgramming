b886b2c380974520e4246624877f14291fa1a13d
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-05-02 20:48:14 -0500

    Renamed SudoExcept.h to sudo_except.h

--- game_engine/include/exceptions/sudo_except.h
+++ game_engine/include/exceptions/sudo_except.h
@@ -1,6 +1,6 @@
 #ifndef EXCEPTIONS_SUDOEXCEPT_H
 #define EXCEPTIONS_SUDOEXCEPT_H
 
-#define throw_trace(ARG) throw GameError(ARG, __FILE__, __LINE__);
+#define THROW_TRACE(ARG) throw GameError(ARG, __FILE__, __LINE__);
 
 #endif
