606f35f035d98763327d07d393156a1a5a61aec8
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-05-02 20:48:46 -0500

    Created a common file for all macros which handle forms of exceptions.

---
+++ game_engine/include/exceptions/except_macros.h
@@ -0,0 +1,22 @@
+#ifndef EXCEPTIONS_EXCEPT_MACROS_H
+#define EXCEPTIONS_EXCEPT_MACROS_H
+
+#include "game_exception.h"
+#include "include/logs/logger.h"
+
+#ifndef THROW_TRACE
+	#define	THROW_TRACE(ARG) throw GameError(ARG, __FILE__, __LINE__);
+#endif
+
+#ifndef ASSERT_LOG
+	#define	ASSERT_LOG(ARG, MSG) if(!expr){Logger::logFailure(__FILE__, __LINE__, MSG);}
+#endif
+
+#ifndef ASSERT_CRITICAL
+	#define	ASSERT_CRITICAL(ARG, MSG) if(!ARG){\
+				Logger::logFailure(__FILE__, __LINE__, MSG);\
+				std::exit(0);\
+			}
+#endif
+
+#endif
